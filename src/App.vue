0a2901df2a4a984057c20a06a790da0e5c8c2b9e
apel-pompi <apel.pompi@gmail.com>
2024-04-04 21:56:10 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    
+    <div>
+      <Header/>
+      <Futter/>
+    </div>
+
+</template>
+
+<script>
+import  Header  from "./components/Header_Futter/Header";
+import  Futter  from "./components/Header_Futter/Futter";
+export default {
+  components:{
+    Header,
+    Futter,
+  }
+  
+}
+</script>
+
---
+++ src/components/Header_Futter/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <p>Header Page</p>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'header-vue-page'
+}
+</script>
